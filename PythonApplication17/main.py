8131ee3416bee44ee50f36acf823d237259e3df2
Farleyn <119632860+Farleyn@users.noreply.github.com>
2025-12-18 12:11:54 +0200

    Refactor main function for improved readability

--- PythonApplication17/main.py
+++ PythonApplication17/main.py
@@ -1,40 +1,28 @@
 from moodul import *
 
-def main():
-    users = []
-    passwords = []
-    current_user = None
+users = []
+passwords = []
+current_user = None
 
-    while True:
-        print("1 - Registreerimine")
-        print("2 - Autoriseerimine")
-        print("3 - Nime/parooli muutmine")
-        print("4 - Unustatud parooli taastamine")
-        print("5 - Lõpetamine")
+while True:
+    print("1 - Registreerimine")
+    print("2 - Autoriseerimine")
+    print("3 - Nime/parooli muutmine")
+    print("4 - Unustatud parooli taastamine")
+    print("5 - Lõpetamine")
 
-        valik = input("\nValik: ")
+    valik = input("Valik: ")
 
-        if valik == "1":
-            registreeri(users, passwords)
-
-        elif valik == "2":
-            current_user = autoriseeri(users, passwords)
-
-        elif valik == "3":
-            if current_user is None:
-                print("\nPole sisse logitud!\n")
-            else:
-                current_user = muuda(users, passwords, current_user)
-
-        elif valik == "4":
-            unustatud_parool(users, passwords)
-
-        elif valik == "5":
-            print("Programmi lõpp.")
-            break
-
+    if valik == "1":
+        registreeri(users, passwords)
+    elif valik == "2":
+        current_user = autoriseeri(users, passwords)
+    elif valik == "3":
+        if current_user is None:
+            print("Pole sisse logitud!")
         else:
-            print("Vale valik!")
-
-if __name__ == "__main__":
-    main()﻿
+            current_user = muuda(users, passwords, current_user)
+    elif valik == "4":
+        unustatud_parool(users, passwords)
+    elif valik == "5":
+        break
